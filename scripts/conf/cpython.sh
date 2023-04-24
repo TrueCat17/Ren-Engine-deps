e071453643fa33624522fcab4d7db639bb61e9e7
TrueCat17 <truecat17@gmail.com>
2023-04-24 04:43:29 +0300

    disable multi-thread python building when using hard optimizations (needs a lot of memory)

--- scripts/conf/cpython.sh
+++ scripts/conf/cpython.sh
@@ -2,8 +2,6 @@
 set -e
 
 export CC="gcc"
-#export CFLAGS="-O2 -flto"
-#export LDFLAGS="-lm -flto -static"
 
 ./configure \
 	--disable-shared \
--- scripts/make/cpython.sh
+++ scripts/make/cpython.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-make -j4 LDFLAGS="-lm"
+tmp="x-flto"
+if [[ "$tmp" == "x" ]]; then
+	threads="-j4" # usual, enable multi-threading
+else
+	threads="-j1" # hard optimizations, needs a lot of memory, disable multi-threading
+fi
+
+make "$threads" LDFLAGS="-lm"
 cp ./libpython3.11.a ../000res
